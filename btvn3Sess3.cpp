70e5d6c2719662d4e6be2ea04ec83ddba05cf86a
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-22 00:18:59 +0700

    Update btvn3Sess3.cpp

--- btvn3Sess3.cpp
+++ btvn3Sess3.cpp
@@ -8,7 +8,7 @@ int main(){
 	float C = 2 * pi * r;
 	printf("chu vi la : %.2f \n", C);
 	
-	float S = (C * C) / 4 * pi;
+	float S = (C * C) / (4 * pi);
 	printf("dien tich la : %.2f \n", S);
 	
 	return 0; 
