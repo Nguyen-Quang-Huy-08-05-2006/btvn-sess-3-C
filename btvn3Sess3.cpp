e1b869b97222b9c3cd2a4df026f1d469c825b65f
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-21 23:44:06 +0700

    Add files via upload

---
+++ btvn3Sess3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	float r;
+	float pi = 3.14;
+	printf("nhap ban kinh duong tron: ");
+	scanf("%.2f", &r);
+	
+	float C = 2 * pi * r;
+	printf("chu vi la : %.2f \n", C);
+	
+	float S = (C * C) / 4 * pi;
+	printf("dien tich la : %.2f \n", S);
+	
+	return 0; 
+}
